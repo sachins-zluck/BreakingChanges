701e0a5dd28ca6de0a4b803ee8d4f2d327cdd057
sachin <sachins@zluck.in>
2024-07-30 18:13:33 +0530

    initial commit

---
+++ android/app/src/main/java/com/breakingchanges/CalendarModule.kt
@@ -0,0 +1,18 @@
+package com.breakingchanges; // replace your-apps-package-name with your app’s package name
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContext
+import com.facebook.react.bridge.ReactContextBaseJavaModule
+import com.facebook.react.bridge.ReactMethod
+import android.util.Log
+
+class CalendarModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+
+    // add to CalendarModule.kt
+    override fun getName() = "CalendarModule"
+    @ReactMethod fun createCalendarEvent(name: String, location: String) {
+
+        Log.d("CalendarModule", "Create event called with name: $name and location: $location")
+    }
+
+    }
---
+++ windows/BreakingChanges/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::BreakingChanges::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::BreakingChanges::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
